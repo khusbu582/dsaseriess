147ccd425a4fdb9f7654952ed3a740e1dfc60cfb
khusbu582 <khushikumari5579@gmail.com>
2025-03-24 09:26:45 +0530

    Update day20.cpp

--- day20.cpp
+++ day20.cpp
@@ -32,8 +32,32 @@ public:
     }
 };
 
-//ques-> 
-//approach->
-//TC=
-//SC=
+//ques->2 sort char by frequency 
+
 //code
+class Solution {
+  public:
+      string frequencySort(string s) {
+          map<char,int> m;
+          vector<pair<int,char>> v;
+          for(int i=0;i<s.size();i++){
+              m[s[i]]++;
+          }
+          for(auto& it:m){
+              v.push_back({it.second,it.first});
+          }
+          sort(v.begin(),v.end(),greater<pair<int,char>>());
+          string ans;
+          for(int i=0;i<v.size();i++){
+            int z=v[i].first;
+            while(z>0){
+              ans.push_back(v[i].second);
+              z--;  
+          }
+      }
+      return ans;
+      }
+  };
+
+// TC:O(nlogn)
+//SC:O(n)
