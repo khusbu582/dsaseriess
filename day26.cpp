0e4c94fd57fbd3f99133c00d665741856b279f44
khusbu582 <khushikumari5579@gmail.com>
2025-03-28 13:06:58 +0530

    Create day26.cpp

---
+++ day26.cpp
@@ -0,0 +1,65 @@
+//ques->1
+/* a Node of the doubly linked list
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+// Function to insert a new node at given position in doubly linked list.
+class Solution {
+  public:
+    // Function to insert a new node at given position in doubly linked list.
+    Node *addNode(Node *head, int pos, int data) {
+         Node* newNode = new Node(data);
+        
+        if (head == nullptr) return newNode;
+
+        Node* temp = head;
+        
+        for (int i = 0; i < pos && temp->next != nullptr; i++) {
+            temp = temp->next;
+        }
+        
+        newNode->next = temp->next;
+        newNode->prev = temp;
+        
+        if (temp->next != nullptr) {
+            temp->next->prev = newNode;
+        }
+        
+        temp->next = newNode;
+        
+        return head;
+    
+    }
+};
+//ques->2
+/* Link list node */
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, Node* head, int key) {
+        // Code here
+        Node* temp = head;
+        
+        while (temp != nullptr) {
+            if (temp->data == key) {
+                return true;
+            }
+            temp = temp->next;
+        }
+        
+        return false;
+    }
+};
